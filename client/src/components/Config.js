e830892d5660fe623e9593098935c2cd6c38a735
ChrisSHDev <kakeochee@naver.com>
2021-05-25 08:22:18 -0700

    Create hero section of the landing page

---
+++ client/src/components/Config.js
@@ -0,0 +1,9 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+export const API_KEY = 'c04776c4ec9461cd1060150c183a4358';

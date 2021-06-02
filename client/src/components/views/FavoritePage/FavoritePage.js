8b837f5c813b136df2c0b6f7f00af4c37fe81e7c
ChrisSHDev <kakeochee@naver.com>
2021-06-02 07:51:43 -0700

    Create favorite movie list and image hover effect

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,19 +1,65 @@
 import Axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './favorite.css';
+import { Popover } from 'antd';
+import { IMAGE_BASE_URL } from '../../Config';
 
 function FavoritePage() {
 
+  const [Favorites, setFavorites] = useState([]);
+
   useEffect(() => {
+    fetchFavoritedMovie();
+  }, [])
+
+  const fetchFavoritedMovie = () => {
     Axios.post('/api/favorite/getFavoritedMovie', { userFrom: localStorage.getItem('userId') })
       .then(response => {
         if (response.data.success) {
+          setFavorites(response.data.favorites);
           console.log(response.data)
         } else {
           alert('fail to get movie info');
         }
       })
-  }, [])
+  }
+
+  const onClickDelete = (movieId, userFrom) => {
+
+    const variables = {
+      movieId,
+      userFrom
+    }
+    console.log('delete clicked');
+    Axios.post('/api/favorite/removeFromFavoritedmovie', variables)
+      .then(response => {
+        if (response.data.success) {
+          fetchFavoritedMovie();
+        } else {
+          alert('Failed to remove favorite.')
+        }
+      })
+  }
+
+  const renderCards = Favorites.map((favorite, index) => {
+
+    const content = (
+      <div>
+        {favorite.moviePost ?
+          <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : "no image"
+        }
+      </div>
+    )
+
+
+    return <tr key={index}>
+      <Popover content={content} vertitle={`${favorite.movieTitle}`}>
+        <td>{favorite.movieTitle}</td>
+      </Popover>
+      <td>{favorite.movieRunTime} mins</td>
+      <td><button onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)}>Remove</button></td>
+    </tr>
+  })
 
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
@@ -29,11 +75,7 @@ function FavoritePage() {
           </tr>
         </thead>
         <tbody>
-
-
-
-
-
+          {renderCards}
         </tbody>
       </table>
     </div>
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -74,4 +74,14 @@ router.post('/getFavoritedMovie', (req, res) => {
 })
 
 
+router.post('/removeFromFavoritedmovie', (req, res) => {
+
+  Favorite.findOneAndDelete({ movieId: req.body.movieId, userFrom: req.body.userFrom })
+    .exec((err, result) => {
+      if (err) return res.status(400).send(err)
+      return res.status(200).json({ success: true })
+    })
+})
+
+
 module.exports = router;
